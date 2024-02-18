408af282962a59517602294b8ae41f176d0e8249
Noah Burns <n14burns@gmail.com>
2024-02-17 20:24:54 -0500

    reskin

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -47,11 +47,13 @@ const App = () => {
             <div className="content-container">
               <div id = "about" className="about-container">
                 <div className="about-title">ABOUT ME</div>
-                <div className="about-image">
-                  <img className="portrait" src={profileImage}></img>
-                </div>
-                <div className="about-description">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla aliquam eum molestias blanditiis, quis rerum. Deleniti doloribus aspernatur repellendus eligendi alias. Voluptates vel ab amet iste excepturi magni incidunt eaque.
+                <div className="about-pair">
+                  <div className="about-image">
+                    <img className="portrait" src={profileImage}></img>
+                  </div>
+                  <div className="about-description">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla aliquam eum molestias blanditiis, quis rerum. Deleniti doloribus aspernatur repellendus eligendi alias. Voluptates vel ab amet iste excepturi magni incidunt eaque.
+                  </div>
                 </div>
               </div>
               <div id = "projects" className="projects-container"> 
