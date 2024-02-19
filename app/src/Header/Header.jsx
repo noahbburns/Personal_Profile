c9b61da3fa6ff2c282256c25c863872e751122bd
Noah Burns <n14burns@gmail.com>
2024-02-18 22:33:12 -0500

    header

--- app/src/Header/Header.jsx
+++ app/src/Header/Header.jsx
@@ -1,44 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link as ScrollLink, Events } from "react-scroll";
+import { motion } from "framer-motion";
+import _debounce from "lodash/debounce";
 import "./Header.css";
-import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
+
+let tabs = [
+  { id: "about", label: "About" },
+  { id: "projects", label: "Projects" },
+  { id: "contact", label: "Contact" },
+];
 
 const Header = () => {
+  const [activeTab, setActiveTab] = useState(tabs[0].id);
+
+  const handleSetActive = (to) => {
+    setActiveTab(to);
+  };
+
+  // Attach the scroll event listener to update the active tab
+  useEffect(() => {
+    const handleScroll = _debounce((to) => {
+      handleSetActive(to);
+    }, 600); // Adjust the debounce delay as needed
+
+    Events.scrollEvent.register("begin", (to, element) => {
+      handleScroll(to);
+    });
+
+    return () => {
+      Events.scrollEvent.remove("begin");
+    };
+  }, []);
+
   return (
     <div className="header-container">
       <div className="header-links">
-        <ScrollLink
-          to="about"
-          spy={true}
-          smooth={true}
-          offset={0}
-          duration={1200}
-          className="link"
-          activeClass="active-link"
-        >
-          About
-        </ScrollLink>
-        <ScrollLink
-          to="projects"
-          spy={true}
-          smooth={true}
-          offset={0}
-          duration={1200}
-          className="link"
-          activeClass="active-link"
-        >
-          Projects
-        </ScrollLink>
-        <ScrollLink
-          to="contact"
-          spy={true}
-          smooth={true}
-          offset={-100}
-          duration={1200}
-          className="link"
-          activeClass="active-link"
-        >
-          Contact
-        </ScrollLink>
+        {tabs.map((tab) => (
+          <ScrollLink
+            key={tab.id}
+            to={tab.id}
+            spy={true}
+            smooth={true}
+            offset={-200}
+            duration={1200}
+            activeClass="active"
+            onSetActive={handleSetActive}
+            className={`tab-button ${
+              activeTab === tab.id ? "active" : "hover"
+            }`}
+          >
+            {activeTab === tab.id && (
+              <motion.span
+                layoutId="bubble"
+                className="bubble"
+                transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
+              />
+            )}
+            {tab.label}
+          </ScrollLink>
+        ))}
       </div>
     </div>
   );
